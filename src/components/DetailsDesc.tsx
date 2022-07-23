ba0e5a4a5d6e88735d4d996cd611bab09b182162
Jose Sanchez <sanchezjoserafael25@gmail.com>
2022-07-23 14:34:23 -0400

    feat: finished Details screen

---
+++ src/components/DetailsDesc.tsx
@@ -0,0 +1,96 @@
+import { Text, View } from "react-native";
+import { useState } from "react";
+import { EthPrice, NFTTitle } from "./SubInfo";
+import { COLORS, SIZES, FONTS } from "../constants";
+import { NFTDataT } from "../types";
+
+interface Props {
+  data: NFTDataT;
+}
+
+const shortText = (text: string) => text.slice(0, 100);
+
+const DetailsDesc = ({ data }: Props) => {
+  const [text, setText] = useState(shortText(data.description));
+  const [readMore, setReadMore] = useState(false);
+
+  const handleReadMorePress = () => {
+    if (readMore === false) {
+      setText(data.description);
+      setReadMore(true);
+
+      return;
+    }
+
+    setText(shortText(data.description));
+    setReadMore(false);
+  };
+
+  return (
+    <>
+      <View
+        style={{
+          width: "100%",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <NFTTitle
+          title={data.name}
+          subTitle={data.creator}
+          titleSize={SIZES.extraLarge}
+          subTitleSize={SIZES.font}
+        />
+        <EthPrice price={data.price} />
+      </View>
+
+      <View
+        style={{
+          marginVertical: SIZES.extraLarge * 1.5,
+        }}
+      >
+        <Text
+          style={{
+            fontSize: SIZES.font,
+            fontFamily: FONTS.semiBold,
+            color: COLORS.primary,
+          }}
+        >
+          Description
+        </Text>
+
+        <View
+          style={{
+            marginTop: SIZES.base,
+          }}
+        >
+          <Text
+            style={{
+              fontSize: SIZES.small,
+              fontFamily: FONTS.regular,
+              color: COLORS.secondary,
+              lineHeight: SIZES.large,
+            }}
+          >
+            {text}
+            {!readMore && "..."}
+
+            <Text
+              style={{
+                fontSize: SIZES.small,
+                fontFamily: FONTS.semiBold,
+                color: COLORS.primary,
+              }}
+              onPress={handleReadMorePress}
+            >
+              {readMore ? " Show Less" : " Show More"}
+            </Text>
+          </Text>
+        </View>
+      </View>
+    </>
+  );
+};
+
+export default DetailsDesc;
